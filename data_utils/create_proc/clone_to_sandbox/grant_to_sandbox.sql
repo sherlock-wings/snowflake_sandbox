6cb1c866cb416bc3542ebf42b01b5b589264004c
sherlock-wings <122579948+sherlock-wings@users.noreply.github.com>
2025-02-28 13:53:20 -0500

    fixed grant_to_sandbox so now it actually works

--- data_utils/create_proc/clone_to_sandbox/grant_to_sandbox.sql
+++ data_utils/create_proc/clone_to_sandbox/grant_to_sandbox.sql
@@ -1,53 +1,56 @@
-use role securityadmin;
-use warehouse sherlockwings_compute_wh;
-
-create or replace procedure sherlockwings_edw_db.util.grant_to_sandbox(SCHEMA_NAME varchar)
-returns varchar
+create or replace procedure sherlockwings_edw_db.util.grant_to_sandbox(TARGET_SCHEMA varchar)
+returns variant
 language javascript
-execute as owner
-as 
+execute as caller 
+as
 $$
-var env_full_ar_name = SCHEMA_NAME.toUpperCase().split('.')[0] + '_FULL_AR';
+var query_counter = 0;
 
-//Remove existing RBAC by applying grants from the Access Definition table for "Full" access rights. These will be given
-//to the Full Access role in the Sandbox.
-//
-//Add to those grants by applying Read-Write access to the sandbox's Read-write role, again using an access definition 
-//table. 
+var role_string = TARGET_SCHEMA.toUpperCase().split('.')[0];
+var read_role = role_string + '_' + 'R_AR';
+var readwrite_role = role_string + '_' + 'RW_AR';
+var full_role = role_string + '_' + 'FULL_AR';
 
-var get_grants_template = "select command_text from def_schema_access_<access-type> where first_time_setup_ind = 'n' order by command_ordinal;";
-var access_types = ['full', 'readwrite', 'read'];
+var roles = {'full': full_role, 'readwrite': readwrite_role, 'read': read_role};
 
-var grants_dict = {};
+var get_grants_template = "select command_text from sherlockwings_edw_db.util.def_schema_access_<access-type> where first_time_setup_ind = false order by command_ordinal";
+var grant_ls = [];
 
-for (let i = 0; i < access_types.length; i++) {
-
-    var qry = snowflake.createStatement( { 
+// Collect into a single list all GRANT statements needed for revocation on Read, Read/Write, and Full access roles 
+for (const access_type in roles) {
+    var get_grants = get_grants_template.replace('<access-type>', access_type);
+    var qry = snowflake.createStatement({sqlText:get_grants});
     
-        sqlText: get_grants_template.replace('<access-type>', access_types[i]) 
-        
-    } );
-
     try {
-        res = qry.execute();
+        var res = qry.execute();
+        query_counter += 1;
+        snowflake.setSpanAttribute("query"+query_counter, qry.getSqlText()); // log all queries executed via telemetry
     }
-
     catch (err) {
         return "Failed executing statement `"+qry.getSqlText()+"`. Error message: `" + err + "`";
     }
-    
-    let grants_ls = [];
 
     while (res.next()) {
-        grants_ls.push(res.getColumnValue(1));
+        var grant_statement = res.getColumnValue(1).replace('<schema-name>', TARGET_SCHEMA);
+        var grant_statement = grant_statement.replace('<' + access_type + '-access-role-name>', roles[access_type]);
+        grant_ls.push(grant_statement);
     }
-
-    grants_dict[ access_types[i] ] = grants_ls;
 }
 
-return JSON.stringify(grants_dict);
+// Grant all access to all roles on target schema
+for (let i = 0; i < grant_ls.length; i++) {
+
+    qry = snowflake.createStatement({sqlText: grant_ls[i]});
+    try {
+        var res = qry.execute();
+        query_counter += 1;
+        snowflake.setSpanAttribute("query"+query_counter, qry.getSqlText());
+    }
+    catch (err) {
+        return "Failed executing statement `"+qry.getSqlText()+"`. Error message: `" + err + "`";
+    }
+}
+
+return "Successfully executed " + grant_ls.length + " SQL statements";
 $$
 ;
-
--- example call
-call grant_to_sandbox('SANDBOX_EDW_DB.RAW');
