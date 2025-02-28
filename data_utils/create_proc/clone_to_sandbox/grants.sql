2af4df67baca214c8ff73b193de115dbffb08d3e
sherlock-wings <122579948+sherlock-wings@users.noreply.github.com>
2025-02-28 14:10:10 -0500

    Update grants.sql

--- data_utils/create_proc/clone_to_sandbox/grants.sql
+++ data_utils/create_proc/clone_to_sandbox/grants.sql
@@ -1,3 +1,4 @@
--- use role securityadmin;
+use role securityadmin;
 
--- grant usage on sherlockwings.util.grant_to_sandbox(
+grant usage on procedure sherlockwings_edw_db.util.grant_to_sandbox(varchar) to role sandbox_sysadmin;
+grant usage on procedure sherlockwings_edw_db.util.revoke_current_access(varchar, varchar) to role sandbox_sysadmin;
