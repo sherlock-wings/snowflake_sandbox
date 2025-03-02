0fc19e83db191a69442306b1c4aa61141938dbe8
sherlock-wings <122579948+sherlock-wings@users.noreply.github.com>
2025-03-01 23:42:58 -0500

    Removing redundant grants and renaming schema_grants.sql

--- data_utils/create_proc/clone_to_sandbox/grants.sql
+++
@@ -1,6 +0,0 @@
-use role securityadmin;
-
-grant usage on database sherlockwings_edw_db to role sandbox_sysadmin;
-grant usage on schema sherlockwings_edw_db.util;
-grant usage on procedure sherlockwings_edw_db.util.grant_to_sandbox(varchar) to role sandbox_sysadmin;
-grant usage on procedure sherlockwings_edw_db.util.revoke_current_access(varchar, varchar) to role sandbox_sysadmin;
---
+++ data_utils/create_proc/clone_to_sandbox/schema_grants.sql
@@ -0,0 +1,4 @@
+use role securityadmin;
+
+grant usage on database sherlockwings_edw_db to role sandbox_sysadmin;
+grant usage on schema sherlockwings_edw_db.util;
