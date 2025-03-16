55a45189d1719fdcef332f22e52472ee53b0eab5
PFCallahan <patrick.callahan@hakkoda.io>
2025-03-16 13:06:49 -0400

    moved all access rights CSVs to RBAC folder-- makes more sense to keep it there than data_utils

--- data_utils/create_table/sproc_executions.sql
+++
@@ -1,7 +0,0 @@
--- use role sherlockwings_admin_fr;
--- use warehouse sherlockwings_compute_wh;
--- use schema sherlockwings_edw_db.util;
-
--- create event table sproc_executions_ev;
-
--- Not doing this anymore-- using Snowflake's internal event table under TELEMETRY
