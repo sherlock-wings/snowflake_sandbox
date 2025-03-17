7d3d6bce3d1079179bfc7f24a7a116602965933a
patrick.f.callahan <patrick.f.callahan@gmail.com>
2025-03-17 11:32:23 -0400

    stepping away to reassess how I can do this without spending too much money

--- models/1_raw/src_fact_weather/get_historical_weather.py
+++ models/1_raw/src_fact_weather/get_historical_weather.py
@@ -8,6 +8,6 @@ if not API_KEY:
     raise ValueError("The API Key for the OpenWeatherMap API is not set in your Environment Variables.")
 
 dat_city = pd.read_csv(PROJECT_DIR+'/models/1_raw/src_dim_city/6_output_src_dim_city.csv')
-# print(cities.head(10))
+print(cities.head(10))
 
 
