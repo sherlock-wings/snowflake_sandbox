f8349f5c1e1cbe522130d325ef0108d4b331eee0
patrick.f.callahan <patrick.f.callahan@gmail.com>
2025-03-19 01:15:28 -0400

    Added login code

--- models/1_raw/src_bsky_posts.py
+++ models/1_raw/src_bsky_posts.py
@@ -1,3 +1,10 @@
-from aproto import Client
+from atproto import Client
+import os
+ 
+USR = os.getenv('BSY_USR').lower()
+KEY = os.getenv('BSY_KEY')
 
-# TODO 
+print(f"Retrieved env vars $USR = {USR} and $KEY = {KEY}")
+
+cli = Client()
+cli.login(USR, KEY)
