a88e6d2d7a654b657ff68bef61578653aa6d169b
patrick.f.callahan <patrick.f.callahan@gmail.com>
2025-03-20 13:26:52 -0400

    Developed new extraction method for bluesky posts

--- models/1_raw/bluesky/src_feed.py
+++ models/1_raw/bluesky/src_feed.py
@@ -4,7 +4,9 @@ import os
 USR = os.getenv('BSY_USR').lower()
 KEY = os.getenv('BSY_KEY')
 
-print(f"Retrieved env vars $USR = {USR} and $KEY = {KEY}")
-
 cli = Client()
 cli.login(USR, KEY)
+
+data = cli.get_timeline(limit=1)
+feed = data.feed
+print(feed[0]['post']['record'])
