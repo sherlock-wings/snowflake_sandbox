f559d71f11669f332e324ec07e597464de75eb37
patrick.f.callahan <patrick.f.callahan@gmail.com>
2025-03-26 10:33:49 -0400

    de-tracking scratch file

--- 03_models/1_raw/tables/posts.sql
+++ 03_models/1_raw/tables/posts.sql
@@ -8,7 +8,7 @@ create table if not exists posts (
 ,quote_count                      varchar
 ,reply_count                      varchar
 ,repost_count                     varchar
-,post_created_timestamp           varchar
+,post_created_timestamp           timestamp_tz(9)
 ,text                             varchar
 ,tags                             varchar
 ,embedded_link_title              varchar
@@ -16,6 +16,6 @@ create table if not exists posts (
 ,embedded_link_uri                varchar
 ,author_username                  varchar
 ,author_displayname               varchar
-,author_account_created_timestamp varchar
-,record_captured_timestamp        varchar
+,author_account_created_timestamp timestamp_tz(9)
+,record_captured_timestamp        timestamp_tz(9)
 );
