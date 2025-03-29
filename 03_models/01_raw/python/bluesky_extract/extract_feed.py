45a43c1dd1d09eacecf265005d6ccb78976810f0
patrick.f.callahan <patrick.f.callahan@gmail.com>
2025-03-29 12:39:17 -0400

    trying to get around record loss, rate limits

--- 03_models/01_raw/python/bluesky_extract/extract_feed.py
+++ 03_models/01_raw/python/bluesky_extract/extract_feed.py
@@ -1,4 +1,5 @@
 from atproto import Client
+from atproto.exceptions import NetworkError 
 from azure.storage.blob import BlobServiceClient
 import csv
 from datetime import datetime
@@ -6,8 +7,27 @@ from dateutil import parser as timestamp_parser
 import os
 import pandas as pd
 import pytz
-from re import fullmatch
+import time
 from typing import Tuple
+
+# schema for all tabular data collected in this file
+schema = {'content_id':                       []
+         ,'post_uri':                         []
+         ,'like_count':                       []
+         ,'quote_count':                      []
+         ,'reply_count':                      []
+         ,'repost_count':                     []
+         ,'post_created_timestamp':           []
+         ,'text':                             []
+         ,'tags':                             []
+         ,'embedded_link_title':              []
+         ,'embedded_link_description':        []
+         ,'embedded_link_uri':                []
+         ,'author_username':                  []
+         ,'author_displayname':               []
+         ,'author_account_created_timestamp': []
+         ,'record_captured_timestamp':        []
+        }
 
 # Instantiate a BlueSky session
 def bluesky_login() -> Tuple[Client, str]:
@@ -40,23 +60,23 @@ def get_followed_users(bsky_client: Client, bsky_handle: str, follows_limit: int
 
 
 # write a chunk of post data to CSV
-def write_chunk(df: str, bsky_username: str, output_path: str='posts_output') -> None:
+def write_chunk(df: pd.DataFrame, output_path: str='posts_output') -> None:
     # filename format is posts_<extraction_date>_<file_ordinal>.csv, where <final ordinal> is an incremental int
     # ex) If 3 files are generated on New Years Day 2025, the names are ['posts_2025-01-01_1.csv', 'posts_2025-01-01_2.csv', 'posts_2025-01-01_3.csv']
     rn = datetime.now().strftime('%Y-%m-%d')
-    last_filenum = -1
+    last_file_num = -1
     filename = f"{output_path}/{rn}_"
 
     if os.path.exists(f"{output_path}/{rn}_1.csv"):
         # get a list of ints where each item is the number just before the '.csv' part in the file name-- get CSV filenames only
         files = [int(file.split('_')[-1].split('.')[0]) for file in os.listdir(output_path) if file.split('.')[-1] == 'csv']
         files.sort()
-        last_filenum = files[-1]+1 #increment by one
+        last_file_num = files[-1]+1 #increment by one
     elif not os.path.exists(output_path): 
         os.makedirs(output_path)
 
-    if last_filenum > 0:
-        filename += f"{last_filenum}.csv"
+    if last_file_num > 0:
+        filename += f"{last_file_num}.csv"
     else:
         filename += "1.csv"
     df.to_csv(df.to_csv(filename,
@@ -67,52 +87,45 @@ def write_chunk(df: str, bsky_username: str, output_path: str='posts_output') ->
                         escapechar='\\',       # Escape special chars
                         doublequote=True,      # Handle existing quotes
                         lineterminator='\n'    # Standard line terminator
-                        )
-                )
+                       )
+              )           
+# check if the current file is already "full" (larger than 100 MB, by default)
+# if it is, stash the current data object as CSV and reset a new empty one    
+def chunk_check(schema_input: dict, filesize_limit_mb: int = 100, dict_input: dict=None, dataframe_input: pd.DataFrame=None) -> Tuple[pd.DataFrame, dict]:
+    if not dict_input and not dataframe_input:
+        raise ValueError("chunk_check() requires an argument for `dict_input` and/or for `dataframe_input`. Both cannot be ommitted.")
+    elif not dataframe_input:
+        dataframe_input = pd.DataFrame(dict_input)
+    size = dataframe_input.memory_usage(deep=True).sum() / (1000000)
+    print(f"Current calculated space of df is {size:,.2f} MB")
+    if size >= filesize_limit_mb:
+        print("SIZE LIMIT TRIGGERED")
+        write_chunk(dataframe_input)
+        return pd.DataFrame(), schema_input
+    return dataframe_input, dict_input
     
 # write User Feed data as a series of one or more CSVs
-def stash_user_posts(bsky_client: Client, bsky_did: str, bsky_username: str, df: pd.DataFrame=None) -> pd.DataFrame:
-    schema = {'content_id':                       []
-             ,'post_uri':                         []
-             ,'like_count':                       []
-             ,'quote_count':                      []
-             ,'reply_count':                      []
-             ,'repost_count':                     []
-             ,'post_created_timestamp':           []
-             ,'text':                             []
-             ,'tags':                             []
-             ,'embedded_link_title':              []
-             ,'embedded_link_description':        []
-             ,'embedded_link_uri':                []
-             ,'author_username':                  []
-             ,'author_displayname':               []
-             ,'author_account_created_timestamp': []
-             ,'record_captured_timestamp':        []
-             }
-    data = schema 
-    
+def stash_user_posts(schema_input: dict, bsky_client:Client, bsky_did:str, bsky_username:str) -> pd.DataFrame:
+    data         = {col: [] for col in schema_input} 
     csr          = None
     pages_remain = True
-    filenum      = 0
     page_num     = 0
-    
+    df           = pd.DataFrame()
+
     # Iterate through every post in their account's post history
     while pages_remain:
-        
-        # check if the current file is already "full" (larger than 100 MB)
-        # if it is, stash the current data object as CSV and reset a new empty one
-        df = pd.DataFrame(data)
-        print(f"Current calculated space of df is {df.memory_usage(deep=True).sum() / (1024*1024)} MB")
-        if df.memory_usage(deep=True).sum() / (1024*1024) >= 100:
-            filenum+=1
-            write_chunk(df, bsky_username)
-            data = schema
-            del df # No need to lock up memory while the next instance of `data` is filling up...
-        
         # Retrieve a paginated post-feed for a specific bluesky user
         page_num += 1  
-        resp      = bsky_client.get_author_feed(actor=bsky_did, cursor=csr)
-        feed      = resp.feed
+        
+        try:
+            resp = bsky_client.get_author_feed(actor=bsky_did, cursor=csr)
+        except NetworkError:
+            print(f"Call for page {page_num} of user @{bsky_username}'s post data was blocked by Rate-Limiting.")
+            print(f"Trying again in 300 seconds...")
+            time.sleep(300)
+            resp = bsky_client.get_author_feed(actor=bsky_did, cursor=csr)
+
+        feed = resp.feed
         print(f"Retrieving post data from page {page_num} for user @{bsky_username}.bsky.social...", end='\r')
         for item in feed:
             #
@@ -155,14 +168,12 @@ def stash_user_posts(bsky_client: Client, bsky_did: str, bsky_username: str, df:
 
             ts = datetime.now(pytz.timezone('America/New_York')).astimezone(pytz.timezone('UTC'))
             data['record_captured_timestamp'].append(ts) 
-            
+
+        _, data = chunk_check(schema_input=schema, dict_input=data)
         if not resp.cursor:
             pages_remain = False
-        
         csr = resp.cursor        # reset cursor when another page of posts is available
-    
-    df = pd.DataFrame(data)
-    return df
+    return pd.DataFrame(data)
 
 # Driver function
 def extract_feed() -> None:
@@ -176,21 +187,24 @@ def extract_feed() -> None:
         c += 1
         print(f"\n\n{str(c).zfill(3)} of {str(len(followed_users)).zfill(3)} | Parsing posts from user @{usr}...")
         # accumulate data across the feeds of many users
-        # stash_user_posts() will save data to CSV once it hits 100 MB threshold
+        # stash_user_posts() will save CSV data should it hit the 100 mb threshold mid-ingestion for a single user
         if c == 1:
             df = stash_user_posts(bsky_client=cli, bsky_did=followed_users[usr][0], bsky_username=usr)
         else:
             df_next = stash_user_posts(bsky_client=cli, bsky_did=followed_users[usr][0], bsky_username=usr)
             df = pd.concat([df, df_next])
+            # if the 100 MB threshold is hit between users, stash the data at this point
+            df, _ = chunk_check(schema_input=schema, dataframe_input=df)
     if len(df) > 0:
-        # ensure residual data is written
-        write_chunk(df, session_usr)
+        # ensure any remaining data less than 100 MB is still written
+        write_chunk(df)
     print(f"Feed Ingestion Complete! Uploading to Azure now...\n")
     files = [file for file in os.listdir('posts_output') if file.endswith('.csv')]
     print(f"{len(files)} total CSV files detected.")
     
     for i in range(len(files)):
-        print(f"Uploading {files[i]}, {i} of {len(files)}")
+        print(f"Uploading {files[i]}, {i+1} of {len(files)}")
+        upload_file_to_azr(files[i])
     
     print(f"File upload complete!")
 
