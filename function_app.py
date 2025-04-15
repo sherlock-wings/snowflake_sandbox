a89088bb0d7115ed9b97f212d93ea2bb1d7a7f50
patrick.f.callahan <patrick.f.callahan@gmail.com>
2025-04-15 15:11:11 -0400

    changing function_app decorators

--- function_app.py
+++ function_app.py
@@ -349,9 +349,11 @@ def get_watermark_table() -> bool:
 # Driver function
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 0 9 */2 * *", arg_name="myTimer", run_on_startup=False,
-              use_monitor=False) 
-def extract_feed(myTimer: func.TimerRequest) -> None:
+@app.function_name(schedule="0 0 9 */2 * *"
+                  ,arg_name="timer_trigger"
+                  ,run_on_startup=False
+                  ,use_monitor=False) 
+def extract_feed(timer_trigger: func.TimerRequest) -> None:
     cli, session_usr = bluesky_login()
     
     # to collect information from bluesky, you need a bluesky client. 
