9b7a1c603226158d8bfc36a450fa2ed83a336c86
patrick.f.callahan <patrick.f.callahan@gmail.com>
2025-04-16 13:14:34 -0400

    imports broke it-- back to foo

--- function_app.py
+++ function_app.py
@@ -1,19 +1,6 @@
-from atproto import Client
-from atproto.exceptions import NetworkError 
-import azure.functions as func
-from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient, ContainerClient
-import csv
-from datetime import datetime
-from dateutil import parser as timestamp_parser
-from io import StringIO, BytesIO
 import logging
-import os
-import pandas as pd
-import pytz
-import time
-from typing import Tuple
-
+import azure.functions as func
+# foo
 app = func.FunctionApp()
 
 @app.timer_trigger(schedule="1 1 1 1 1 1", arg_name="myTimer", run_on_startup=False,
