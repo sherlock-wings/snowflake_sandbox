734147e8be198b5f77174636e8910ed4d4fd82e8
patrick.f.callahan <patrick.f.callahan@gmail.com>
2025-06-02 11:21:53 -0400

    rescheduled task
    now the task always executes between ingestions-- outside of DST, it executes at 2 AM Eastern time. During DST, it executes at 1 AM eastern time. Now neither of these times overlaps with raw file ingestion, which would have been the case once DST is over had I not fixed this

--- 03_models/01-ingestion/extract_firehose/task_process_firehose_data.sql
+++ 03_models/01-ingestion/extract_firehose/task_process_firehose_data.sql
@@ -2,7 +2,7 @@ use role ADMIN_FR;
 
 create or replace task bluesky_db.main.task_process_firehose_data
 warehouse = compute_wh
-schedule = 'USING CRON 0 5 * * * UTC'
+schedule = 'USING CRON 0 6 * * * UTC'
 as 
 call bluesky_db.main.process_firehose_data();
 ALTER TASK bluesky_db.main.task_process_firehose_data RESUME;
