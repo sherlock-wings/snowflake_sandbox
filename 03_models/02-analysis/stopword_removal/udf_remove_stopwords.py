74caa28062d138be154f27fda7a3b14aead656b4
patrick.f.callahan <patrick.f.callahan@gmail.com>
2025-06-14 23:49:52 -0400

    bad filename

--- 03_models/02-analysis/stopword_removal/udf_remove_stopwords.py
+++ 03_models/02-analysis/stopword_removal/udf_remove_stopwords.py
@@ -1,16 +1,15 @@
 import re
 
 def remove_stopwords(stopword_list: list, POST_TEXT: str):
-    special_char_pat = r'[\`\~\!\@\#\$\%\^\&\*\(\)\-\_\+\=\/\<\>\,\.\|\[\]\{\}]+'
+    special_char_pat = r'[\\\`\~\!\@\#\$\%\^\&\*\(\)\-\_\+\=\/\<\>\,\.\|\[\]\{\}]+'
     POST_TEXT = re.sub(special_char_pat, '', POST_TEXT).lower().split()
     txt_ls = []
     for i in range(len(POST_TEXT)):
         if "'" in POST_TEXT[i]:
             pair = POST_TEXT[i].split("'")
-            newval_1 = pair[0]
-            newval_2 = "'" + pair[1]
-            txt_ls.append(newval_1)
-            txt_ls.append(newval_2)
+            newval = "'" + pair[1]
+            txt_ls.append(pair[0])
+            txt_ls.append(newval)
         else:
             txt_ls.append(POST_TEXT[i])
     return [word for word in txt_ls if word not in stopword_list]
