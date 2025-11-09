d26070af3804a62a1c90d915a96b1c7e6787fae9
patrick.f.callahan <patrick.f.callahan@gmail.com>
2025-11-08 22:54:25 -0500

    continuing with my approach

---
+++ 03_models/02-analysis/wordtrends/02_aggregate_ngrams.sql
@@ -0,0 +1,15 @@
+insert into bluesky_db.pfc.ngram_counts
+select MONTH_CREATED_AT
+      ,N_GRAM
+      ,N_GRAM_SIZE
+      ,SENTIMENT_DETECTED_LABEL
+      ,min(sentiment_confidence_score) as minimum_confidence_scoree
+      ,max(sentiment_confidence_score) as maximum_confidence_scoree
+      ,avg(SENTIMENT_CONFIDENCE_SCORE) as average_sentiment_score
+      ,count(distinct content_id) as total_distinct_posts
+      ,count(*) as total_occurrences 
+from post_ngrams
+where MONTH_CREATED_AT < date_trunc(month, current_date())
+  and month_created_at > nvl((select max(month_created_at) from bluesky_db.pfc.ngram_counts), to_timestamp_tz('1900-01-01 00:00:00 +0000'))
+group by all 
+;
